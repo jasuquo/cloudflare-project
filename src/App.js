2d9256030a392211c95d8f613bf12d81b23948b8
joemile <joemile@macs-MacBook-Air.local>
2022-07-19 14:10:13 +0100

    edit App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          An commit Edit in <code>src/App.js</code> 
         </p>
         <a>
           Welcome to a sample react app deployed on cloudflare
